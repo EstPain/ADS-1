017e0ca57867738a54e444f3afc091e18d4fb017
EstPain <99329093+EstPain@users.noreply.github.com>
2022-05-15 13:52:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,49 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
+using namespace std;
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+for (int i = 2; i < value; i++) {
+if (value % i == 0)
+return false;
+}
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t a = 0;
+uint64_t i = 2;
+while (a < n) {
+if (checkPrime(i)) {
+a++;
+}
+i++;
+}
+return i - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+while (true) {
+value++;
+if (checkPrime(value)) {
+break;
+}
+}
+return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sum = 0;
+for (int i = 2; i < hbound; i++) {
+if (checkPrime(i) == 1)
+sum += i;
+}
+return sum;
+}
+
+int main() {
+    int a=7;
+    cout<<checkPrime(a)<<endl;
+    return 0;
 }
