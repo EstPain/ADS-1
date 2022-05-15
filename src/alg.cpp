594187b7327a91e0370c6df1bcba129ce5a20bdd
EstPain <99329093+EstPain@users.noreply.github.com>
2022-05-15 14:09:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,42 +4,42 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-for (int i = 2; i < value; i++) {
-if (value % i == 0)
-return false;
-}
-return true;
+  for (int i = 2; i < value; i++) {
+    if (value % i == 0)
+    return false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-uint64_t a = 0;
-uint64_t i = 2;
-while (a < n) {
-if (checkPrime(i)) {
-a++;
-}
-i++;
-}
-return i - 1;
+  uint64_t a = 0;
+  uint64_t i = 2;
+  while (a < n) {
+    if (checkPrime(i)) {
+      a++;
+    }
+    i++;
+  }
+  return i - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-while (true) {
-value++;
-if (checkPrime(value)) {
-break;
-}
-}
-return value;
+  while (true) {
+    value++;
+    if (checkPrime(value)) {
+      break;
+    }
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-uint64_t sum = 0;
-for (int i = 2; i < hbound; i++) {
-if (checkPrime(i) == 1)
-sum += i;
-}
-return sum;
+  uint64_t sum = 0;
+  for (int i = 2; i < hbound; i++) {
+    if (checkPrime(i) == 1)
+      sum += i;
+  }
+  return sum;
 }
 
 int main() {
